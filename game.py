3b1c59295159eb48cc0c43df1276fd796dabc6e2
Diego Fonseca <dfonseca@terpmail.umd.edu>
2025-04-08 16:54:34 -0400

    added a method to let player choose difficulty

---
+++ game.py
@@ -0,0 +1,8 @@
+class game:
+     def difficulty(option):
+         """This function will let the user choose the difficulty.
+        The three options are easy, medium, hard.
+        Easy option will require the player to asnwer 10/20 to win
+        Medium will require 15/20 to win
+        Hard will require 18/20 to win"""
+        
