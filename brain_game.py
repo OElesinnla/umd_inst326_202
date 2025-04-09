4d119f1e8eb8f9313c2f693b0d27775d4f83bcd0
Oluwanifemi Elesinnla <oelesinn@terpmail.umd.edu>
2025-04-08 22:43:10 -0400

    Deleted the game.py file. Still thinking about how to change our game and make it more complex

--- brain_game.py
+++ brain_game.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-
+" A Trivia Game with sports, history and math related questions." 
 
 class Question:
     """
@@ -8,8 +7,10 @@ class Question:
     Attributes:
         prompt (str): The text of the question.
         answer (str): the correct answer to the question.
-        category (str): the category of the question as in math, trivia, sports, history.
-        difficulty (str): the difficulty level of the question ('easy', 'medium', 'hard').
+        category (str): the category of the question as in math, trivia, 
+        sports, history.
+        difficulty (str): the difficulty level of the question ('easy', 
+        'medium', 'hard').
     
     """
     def __init__(self, prompt, answer, category, difficulty):
@@ -17,6 +18,21 @@ class Question:
         self.answer = answer
         self.category = category
         self.difficulty = difficulty
-=======
-" A Trivia Game with sports, history and math related questions." 
->>>>>>> 3ad027b41b4cadff3e23ad335bff415980f4029d
+        
+    def score():
+      pass
+    
+class game:
+     def difficulty(option):
+         """This function will let the user choose the difficulty.
+        The three options are easy, medium, hard.
+        Easy option will require the player to asnwer 10/20 to win
+        Medium will require 15/20 to win
+        Hard will require 18/20 to win"""
+     def fail(self):
+        """This method will check if the player did not achieve the goal from 
+        the desired difficulty.
+        If chose easy and got less than 10/20 then fail and print message
+        if chose medium and got less than 15/20 then fail
+        If chose hard and got less than 18/20 then fail
+        will check after all questions have been answered"""
