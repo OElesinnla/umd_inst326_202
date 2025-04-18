a79c904804403792b285e266bb16c80783efa1e7
olnume <chikenmastar@gmail.com>
2025-04-18 10:50:45 -0400

    removed 'Adder' example class

--- model.py
+++ model.py
@@ -1,16 +1,3 @@
-class Adder:
-    def __init__(self):
-        self.values = []
-
-    def prompt(self):
-        res = input()
-        self.values.append(int(res.strip()))
-
-    def get_sum(self):
-        return sum(self.values)
-    
-    
-    
 from cards import Card
 
 
