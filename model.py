b9d17668a9ad5f62142bcc22eb66ec3389421559
Oluwanifemi Elesinnla <oelesinn@terpmail.umd.edu>
2025-04-20 23:38:21 -0400

    validate input function edit

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ FULL_SUIT: set[Card] = {Card(i) for i in range(13)}
 def validate_input(player_input, hand_cards):
     if player_input.isalpha == False:
         raise ValueError ("Invalid input")
-    if not isinstance (player_input, card.alias):
+    if not isinstance (player_input, Card.alias):
         raise ValueError
     if player_input not in hand_cards:
         raise ValueError ("Not in your deck")
