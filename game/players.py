0e07b604d996214c3bc361d797c196e2652576a5
Oluwanifemi Elesinnla <oelesinn@terpmail.umd.edu>
2025-05-01 16:31:00 -0400

    tested the validate input method changed the card alias to the right number

--- game/players.py
+++ game/players.py
@@ -1,4 +1,4 @@
-from cards import Card, to_card_value, get_card_by_alias
+from cards import Card, to_card_value
 
 
 class Player:
@@ -18,26 +18,24 @@ class Player:
 class HumanPlayer(Player):
     """A player of the game who is using the terminal interface.
     """
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name: str, hand: set[Card]):
+        super().__init__(name, hand)
 
     def turn(self) -> Card:
         p_in = input().strip() # Text prompt will be handled by the viewer
-        if not self.validate_input(p_in, self.hand):
+        if not (card := self.validate_input(p_in)):
             return None
-        card = get_card_by_alias(p_in)
-        self.hand -= card
-        return card
+        else:
+            self.hand -= card
+            return card
 
     def validate_input(self, player_input: str) -> bool:
         player_input = player_input.strip()
-        if player_input.isalpha() == False:
-            return False
-        elif not player_input in \
+        if not player_input in \
             ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 
             'Jack', 'Queen', 'King'):
-            return False
-        elif to_card_value(player_input) not in self.hand_cards:
-            return False
+            return None
+        elif to_card_value(player_input) not in self.hand:
+            return None
         else:
-            return True
+            return to_card_value(player_input)
