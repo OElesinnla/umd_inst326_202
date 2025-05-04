180f2d9c49f584455084f89d7d322f68a85b20bb
olnume <chikenmastar@gmail.com>
2025-05-04 03:05:04 -0400

    began Screen and Element classes of viewer.. they are yet to be capable of rendering & need docstrings

--- viewer/view.py
+++ viewer/view.py
@@ -1,5 +1,54 @@
 import os
 
+
+class ScreenElement:
+    """A text sprite to display on the Screen.
+    """
+    def __init__(self, sprite: str, vpos: int, hpos: int, layer: int = 0):
+        self.sprite = sprite
+        self.vpos = vpos
+        self.hpos = hpos
+        lines = sprite.split("\n")
+        self.width = len(lines[0])
+        self.height = len(lines)
+        self.layer = layer
+
+    def __call__(self, *args, **kwds):
+        ...
+
+    def __le__(self, other: 'ScreenElement'):
+        """Determine if self or other is higher-priority in the draw order.
+        """
+        return self.layer <= other.layer
+
+class Screen:
+    """A screen which displays graphical elements. User input will always
+    terminate on the bottom-rightmost line. 
+    """
+    def __init__(self):
+        self.elements = []
+        self.vertical_resolution = 40
+        self.horizontal_resolution = 80
+
+    def render(self):
+        os.system('cls')
+        screen = ''
+        for vp in range(self.vertical_resolution):
+            for hp in range(self.horizontal_resolution):
+                for e in self.elements:
+                    if (vp - e.vpos >= 0 and e.height + e.vpos <= vp) \
+                            and \
+                            (hp - e.hpos > 0 and e.width + e.hpos <= hp):
+                        char = e(vp - e.vpos, hp - e.hpos)
+                screen += char
+            screen += "\n"
+        print(screen) # TODO: account for whitespace buffer
+
+    def append(self, element: ScreenElement) -> None:
+        self.elements.append(element)
+        self.elements.sort()
+
+
 def pclear():
     os.system('cls')
     # DEBUG: 80col ruler
